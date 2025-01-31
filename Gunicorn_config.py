b032690304f5eb77343671f06c60a3a067e19c29
Moeus <2662717402@qq.com>
2025-01-31 14:01:38 +0800

    gunicorn

---
+++ Gunicorn_config.py
@@ -0,0 +1,6 @@
+workers = 9
+threads = 1
+bind = '0.0.0.0:8000'
+proc_name = 'DCNN_app'
+accesslog = '/home/moeus/DCNN_backend/access.log'
+errorlog ='/home/moeus/DCNN_backend/error.log'
--- app.py
+++ app.py
@@ -22,4 +22,5 @@ def predict():
     return json.dumps(result)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    # app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run()
